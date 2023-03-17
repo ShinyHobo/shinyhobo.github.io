1c8c9c93fe67aa0e6f43026e7f66df1f5bbb61d8
ShinyHobo <shinyhobo1@gmail.com>
2023-03-16 19:59:17 -0500

    no message

--- tests/database-browser-src/webpack.config.js
+++ tests/database-browser-src/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     output: {
       filename: "bundle.js",
       //path: path.resolve(__dirname, "dist"),
-      publicPath: '/'
+      //publicPath: '/'
     },
     devServer: {
       historyApiFallback: true,
