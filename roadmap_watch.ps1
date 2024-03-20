fd2c77c749baefb62ccdecec0d9e57bdb8b22725
ShinyHobo <ShinyHobo@users.noreply.github.com>
2024-03-20 22:11:39 +0000

    deploy: 3691818c6eecdd0bdc1e8d52c35704fa6af8ba42

--- roadmap_watch.ps1
+++ roadmap_watch.ps1
@@ -1,6 +1,8 @@
-# powershell -ExecutionPolicy Bypass -File script.ps1
+# powershell -ExecutionPolicy Bypass -File roadmap_watch.ps1
+$userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0"
+$header = @{"accept"="text/html"}
 for(;;) {
-    $content = (wget https://robertsspaceindustries.com/roadmap).Content
+    $content = (wget https://robertsspaceindustries.com/roadmap -UserAgent $userAgent -Headers $header).Content
     if($content.Contains("Currently down for maintenance")) {
         echo "Down"
 		Start-Sleep -s 15
